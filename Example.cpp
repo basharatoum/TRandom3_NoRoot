347f3b42269a1b2ca7fa84225a2ebde3d1915bd6
basharatoum <atoumbashar@gmail.com>
2019-12-20 22:01:37 -0600

    Added XORSHIFT256+ for comaprision

--- Example.cpp
+++ Example.cpp
@@ -2,13 +2,16 @@
 #include "Random.h"
 #include <ctime>
 #include <stdlib.h>
+#include "XorShift256.h"
+
 
 namespace Garfield{
     void P(){
+        //TRANDOM3
     double t;
     double f = RndmUniform();
     int fi=0;
-    for(int i=0;i<1000000000;i++){
+    for(int i=0;i<100000000;i++){
         t = RndmUniform();
         if(f!=t){
             fi++;
@@ -21,11 +24,12 @@ namespace Garfield{
     }
 
     void G(){
+        //DRAND48
     double t;
     //srand48(234953453);
     double f = drand48();
     int fi = 0;
-    for(int i=0;i<1000000000;i++){
+    for(int i=0;i<100000000;i++){
         t = drand48();
         if(f!=t){
             fi++;
@@ -37,17 +41,36 @@ namespace Garfield{
     }
     }
 }
+//extern uint64_t s[4] = {32452345,3453452123,4545645,2342314};
 int main(){
     clock_t time_req;
 	// Using pow function
 	time_req = clock();
     Garfield::P();
     time_req = clock() - time_req;
-    std::cout<< "1 000 000 000 random numbers (TRANDOM) took "<<(float)time_req/CLOCKS_PER_SEC<<" Seconds"<<std::endl;
+    std::cout<< "100 000 000 random numbers (TRANDOM) took "<<(float)time_req/CLOCKS_PER_SEC<<" Seconds"<<std::endl;
     time_req = clock();
     Garfield::G();
     time_req = clock() - time_req;
-    std::cout<< "1 000 000 000 random numbers (drand) took "<<(float)time_req/CLOCKS_PER_SEC<<" Seconds"<<std::endl;
+    std::cout<< "100 000 000 random numbers (drand) took "<<(float)time_req/CLOCKS_PER_SEC<<" Seconds"<<std::endl;
+    
 
+    //XORSHIFT 256+
+    time_req = clock();
+    double aa  = next();
+    double f = aa;
+    int fi=0;
+    for(int i=0;i<100000000;++i){
+        double a  = next();
+        if(f!=a){
+            fi++;
+        }
+        else{
+            std::cout<<f<<" " << fi<<std::endl;
+            f=0.0;
+        }    }
+    time_req = clock() - time_req;
+    std::cout<< "100 000 000 random numbers (XorShitft256) took "<<(float)time_req/CLOCKS_PER_SEC<<" Seconds"<<std::endl;
+    
     return 0;
 }
